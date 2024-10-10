a43814099d779469e8236427ba8888e83b6e907a
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2024-10-10 10:42:55 -0500

    restructure files and draft quash.c

---
+++ new/src/execute.h
@@ -0,0 +1,10 @@
+/*
+Authors: Holden Vail, Blake Carlson
+Date: 10-09-2024
+Class: EECS 678
+Assignment: Quash Project
+File : execute.h
+Description: This is the header file for execute.c. It contains relevant function prototypes for the programs to share.
+*/
+
+int execute_function(char* tokens);
